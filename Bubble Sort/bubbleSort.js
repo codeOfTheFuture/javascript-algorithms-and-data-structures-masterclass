1b7487a7bd66d587c3ec2d6510683a0ebcd40ef6
Jeff Oliver <jeffoliver3384@gmail.com>
2020-03-09 18:06:27 -0400

    bubble sort completed

--- Bubble Sort/bubbleSort.js
+++ Bubble Sort/bubbleSort.js
@@ -1,31 +1,65 @@
 // Bubble Sort
 // A sorting algorithm where the largest values bubble up to the top!
 
-myArray = [5, 2, 4, 9, 6, 1, 0];
+myArray = [37, 45, 29, 8];
 
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     // Start looping with a variable called i at the end of the array towards the beginning
+//     for (let j = 0; j < i - 1; j++) {
+//       // Start an inner loop with a variable called j from the beginning until i -1
+
+//       if (arr[j] > arr[j + 1]) {
+//         // If arr[j] is greater than arr[j+1], swap those two values!
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   // Return the sorted array
+
+//   return arr;
+// }
+
+// Optimized with noSwaps
 function bubbleSort(arr) {
-  let i = arr.length;
-  while (i > 0) {
-    // Start looping with a variable called i at the end of the array towards the beginning
-
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-      // Start an inner loop with a variable called j from the beginning until i -1
-
+      // console.log(arr);
       if (arr[j] > arr[j + 1]) {
-        // If arr[j] is greater than arr[j+1], swap those two values!
-        i = arr.length;
-        temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
-      } else {
-        i -= 1;
+        // Swap!
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
-
-    // Return the sorted array
-
-    return arr;
+    if (noSwaps) break;
   }
+  return arr;
 }
 
-console.log(bubbleSort(myArray));
+// Using a while loop
+// function bubbleSort(arr) {
+//   let swap = arr.length;
+//   while (swap > 0) {
+//     for (let i = 0; i < arr.length; i++) {
+//       n = i + 1;
+//       if (n <= arr.length - 1 && arr[i] > arr[n]) {
+//         swap = arr.length;
+//         x = arr[i];
+//         arr[i] = arr[n];
+//         arr[n] = x;
+//       } else {
+//         swap -= 1;
+//       }
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(bubbleSort(myArray));
