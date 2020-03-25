cb89f059e3320f384c254498152b9b6ce66da024
Jeff Oliver <jeffoliver3384@gmail.com>
2020-03-25 17:16:35 -0400

    depth first search in order

--- Tree Traversal/Depth First InOrder/dfsInOrder.js
+++ Tree Traversal/Depth First InOrder/dfsInOrder.js
@@ -9,22 +9,22 @@ bst.insert(3);
 bst.insert(8);
 bst.insert(20);
 
-// DFS Post-order steps - recursively
-function dfsPostOrder(bst) {
+// Depth First In-Order Steps - Recursively
+function dfsInOrder(bst) {
   // Create a variable to store the values of nodes visited
   const result = [];
   // Write a helper function which accepts a node
   const traverse = node => {
     // If the node has a left property, call the helper function with the left property on the node
     if (node.left) traverse(node.left);
-    // If the node has a right property, call the helper function with the right property on the node
-    if (node.right) traverse(node.right);
     // Push the value of the node to the variable that stores the values
     result.push(node.value);
+    // If the node has a right property, call the helper function with the right property on the node
+    if (node.right) traverse(node.right);
   };
   // Invoke the helper function with the current variable
   traverse(bst.root);
   return result;
 }
 
-console.log(dfsPostOrder(bst));
+console.log(dfsInOrder(bst));
