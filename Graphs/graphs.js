64b658a74fe4b17962c58a2f5bc373d801c37e44
Jeff Oliver <jeffoliver3384@gmail.com>
2022-04-25 22:15:57 -0400

    Graphs

---
+++ Graphs/graphs.js
@@ -0,0 +1,44 @@
+// Adjacency List
+// Can take up less space(in sparse graphs)
+// Faster to iterate over all edges
+// Can be slower to lookup specific edge
+
+// Adjacency Matrix
+// Takes up more space(in sparse graphs)
+// Slower to iterate over all edges
+// Faster to lookup specific edge
+
+//  Adjacency List Graph
+//  Undirected Graph
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
